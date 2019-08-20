3ebb84088968b93d1712d65af725b1517ddc571b
jrocha <jrocha@usbr.gov>
2019-08-20 15:43:19 -0600

    Forgot to add new forms for the new Flood Control feature...

---
+++ FcPlot/FcOpsMenu.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace FcPlot
+{
+    partial class FcOpsMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FcOpsMenu));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxInflowCode = new System.Windows.Forms.TextBox();
+            this.textBoxOutflowCode = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxSpace = new System.Windows.Forms.TextBox();
+            this.textBoxInflowYears = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxOutflowYears = new System.Windows.Forms.TextBox();
+            this.textBoxDams = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDownInflowYear = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownOutflowYear = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownInflowYear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownOutflowYear)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Operate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Inflow Data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Outflow Data";
+            // 
+            // textBoxInflowCode
+            // 
+            this.textBoxInflowCode.Enabled = false;
+            this.textBoxInflowCode.Location = new System.Drawing.Point(103, 51);
+            this.textBoxInflowCode.Name = "textBoxInflowCode";
+            this.textBoxInflowCode.Size = new System.Drawing.Size(125, 22);
+            this.textBoxInflowCode.TabIndex = 3;
+            // 
+            // textBoxOutflowCode
+            // 
+            this.textBoxOutflowCode.Enabled = false;
+            this.textBoxOutflowCode.Location = new System.Drawing.Point(103, 92);
+            this.textBoxOutflowCode.Name = "textBoxOutflowCode";
+            this.textBoxOutflowCode.Size = new System.Drawing.Size(125, 22);
+            this.textBoxOutflowCode.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Max Space";
+            // 
+            // textBoxSpace
+            // 
+            this.textBoxSpace.Enabled = false;
+            this.textBoxSpace.Location = new System.Drawing.Point(103, 13);
+            this.textBoxSpace.Name = "textBoxSpace";
+            this.textBoxSpace.Size = new System.Drawing.Size(125, 22);
+            this.textBoxSpace.TabIndex = 6;
+            // 
+            // textBoxInflowYears
+            // 
+            this.textBoxInflowYears.Location = new System.Drawing.Point(450, 51);
+            this.textBoxInflowYears.Name = "textBoxInflowYears";
+            this.textBoxInflowYears.Size = new System.Drawing.Size(140, 22);
+            this.textBoxInflowYears.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(243, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Year";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(243, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Year";
+            // 
+            // textBoxOutflowYears
+            // 
+            this.textBoxOutflowYears.Location = new System.Drawing.Point(450, 90);
+            this.textBoxOutflowYears.Name = "textBoxOutflowYears";
+            this.textBoxOutflowYears.Size = new System.Drawing.Size(140, 22);
+            this.textBoxOutflowYears.TabIndex = 9;
+            // 
+            // textBoxDams
+            // 
+            this.textBoxDams.Enabled = false;
+            this.textBoxDams.Location = new System.Drawing.Point(304, 12);
+            this.textBoxDams.Name = "textBoxDams";
+            this.textBoxDams.Size = new System.Drawing.Size(140, 22);
+            this.textBoxDams.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(243, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Dam(s)";
+            // 
+            // numericUpDownInflowYear
+            // 
+            this.numericUpDownInflowYear.Location = new System.Drawing.Point(304, 51);
+            this.numericUpDownInflowYear.Name = "numericUpDownInflowYear";
+            this.numericUpDownInflowYear.Size = new System.Drawing.Size(140, 22);
+            this.numericUpDownInflowYear.TabIndex = 13;
+            // 
+            // numericUpDownOutflowYear
+            // 
+            this.numericUpDownOutflowYear.Location = new System.Drawing.Point(304, 92);
+            this.numericUpDownOutflowYear.Name = "numericUpDownOutflowYear";
+            this.numericUpDownOutflowYear.Size = new System.Drawing.Size(140, 22);
+            this.numericUpDownOutflowYear.TabIndex = 14;
+            // 
+            // FcOpsMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 287);
+            this.Controls.Add(this.numericUpDownOutflowYear);
+            this.Controls.Add(this.numericUpDownInflowYear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxDams);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxOutflowYears);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxInflowYears);
+            this.Controls.Add(this.textBoxSpace);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxOutflowCode);
+            this.Controls.Add(this.textBoxInflowCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(467, 334);
+            this.Name = "FcOpsMenu";
+            this.Text = "Flood Control Operations Menu";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownInflowYear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownOutflowYear)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxInflowCode;
+        private System.Windows.Forms.TextBox textBoxOutflowCode;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxSpace;
+        private System.Windows.Forms.TextBox textBoxInflowYears;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxOutflowYears;
+        private System.Windows.Forms.TextBox textBoxDams;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDownInflowYear;
+        private System.Windows.Forms.NumericUpDown numericUpDownOutflowYear;
+    }
+}
---
+++ FcPlot/FcOpsMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Reclamation.TimeSeries.Hydromet.Operations;
+
+namespace FcPlot
+{
+    public partial class FcOpsMenu : Form
+    {
+        FcPlotUI ui;
+        FloodControlPoint pt;
+        public FcOpsMenu(FcPlotUI ui)
+        {
+            InitializeComponent();
+
+            this.ui = ui;
+            pt = new FloodControlPoint(ui.comboBoxSite.Text.ToString());
+            double maxSpace = pt.TotalUpstreamActiveSpace;
+            this.textBoxSpace.Text = maxSpace.ToString("F0");
+            string dams =string.Join(", ", pt.UpstreamReservoirs);
+            this.textBoxDams.Text = dams;
+            string inflowCode = pt.StationQD + " QU";
+            this.textBoxInflowCode.Text = inflowCode;
+            this.textBoxInflowYears.Text = (DateTime.Now.Year - 1).ToString("F0");
+            this.numericUpDownInflowYear.Maximum = (DateTime.Now.Year - 1);
+            this.numericUpDownInflowYear.Value = (DateTime.Now.Year - 1);
+            string outflowCode = pt.StationQD + " QD";
+            this.textBoxOutflowCode.Text = outflowCode;
+            this.textBoxOutflowYears.Text = (DateTime.Now.Year - 1).ToString("F0");
+            this.numericUpDownOutflowYear.Maximum = (DateTime.Now.Year - 1);
+            this.numericUpDownOutflowYear.Value = (DateTime.Now.Year - 1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ui.hydrometChart1.FcOps(ui, this.textBoxInflowCode.Text, this.textBoxOutflowCode.Text,
+                this.numericUpDownInflowYear.Value.ToString(), this.numericUpDownOutflowYear.Value.ToString(),
+                //this.textBoxInflowYears.Text, this.textBoxOutflowYears.Text, 
+                pt.UpstreamReservoirs);
+        }
+    }
+}
